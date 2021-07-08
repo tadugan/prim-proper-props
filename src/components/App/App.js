e53c1feca5cddb1a2dc7a6a7efd41daa821483b2
Tim Dugan <tadugan@me.com>
2021-07-08 13:31:43 -0500

    Refactored Header into a new component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
+// Component Imports
+import Header from '../Header/Header';
+
 function App() {
   let [guestList, setGuestList] = useState([]);
   let [newGuestName, setNewGuestName] = useState('');
@@ -53,9 +56,7 @@ function App() {
   console.log(newGuestMeal)
   return (
     <div className="App">
-      <header>
-        <h1>Prim Proper Props</h1>
-      </header>
+      <Header />
       <h2>Party Leader</h2>
       {guestList[0] && <h3>{guestList[0].name}</h3>}
       <h2>Add a new guest</h2>
